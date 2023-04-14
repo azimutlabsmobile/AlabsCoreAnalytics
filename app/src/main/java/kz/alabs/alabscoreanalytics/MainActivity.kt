776fb01d7cb7c1fce982d419906abebb81ef0d96
Dimash <intern1@crystalspring.kz>
2023-04-14 12:46:37 +0600

    init commit

---
+++ app/src/main/java/kz/alabs/alabscoreanalytics/MainActivity.kt
@@ -0,0 +1,10 @@
+package kz.alabs.alabscoreanalytics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
